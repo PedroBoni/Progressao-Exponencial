20a68089c7cd403f8516b42c168a73c068bd87e5
Pedro Bonifácio <pedro.henrique1210@outlook.com>
2020-03-03 21:59:55 -0300

    First Commit

---
+++ 120.py
@@ -0,0 +1,10 @@
+﻿#Autores: 1DSI3 
+#@Pedro Bonifácio
+#Feito em 03/03/2020
+#Instituição: ETEC
+x = 0 #Declaração de controlador
+valor = 2 #Declaração de Variavel valor
+while (x < 5):
+	valor = valor * valor
+	x = x + 1 #Laço de repetição
+print(valor) #Exibir variavel valor
